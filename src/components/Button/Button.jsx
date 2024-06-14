152ff24f3f8837a89a5c8abf3d5410bd7ef0d51d
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-15 02:55:00 +0530

    created the button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = ({children, handleClick, style = 'primary', shadow = false, type = 'button'}) => {
+    
+  return (
+    <button
+        type={type}
+        onClick={handleClick}
+        className={`${styles.button} ${styles[style]} ${shadow && styles.shadow}`}
+    >
+        {children}
+    </button>
+  )
+}
+
+export default Button
