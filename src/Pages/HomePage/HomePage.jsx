a96b2d84d7ed75f09227d30bebc36823e3905a1b
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-15 02:56:38 +0530

    main page which will contain every component in an orderly manner

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './HomePage.module.css'
+
+
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+        <h1>Expense Tracker</h1>
+
+        
+    </div>
+  )
+}
+
+export default HomePage
