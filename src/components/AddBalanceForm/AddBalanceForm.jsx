7f572a65748432855074b0a69cc8d5faab8c3acc
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-18 02:11:14 +0530

    Add Balance Modal Completed

---
+++ src/components/AddBalanceForm/AddBalanceForm.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import styles from './AddBalanceForm.module.css'
+import Button from '../Button/Button'
+import { useSnackbar } from 'notistack'
+
+
+const AddBalanceForm = ({setIsOpen, setBalance}) => {
+
+    const [income, setIncome] = useState('')
+    const { enqueueSnackbar } = useSnackbar();
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+
+        if(Number(income) < 0){
+            enqueueSnackbar("Income should be greater than 0", {variant: "warning"});
+            setIsOpen(false);
+            return
+        }
+
+        setBalance(prev => prev + Number(income));
+        setIsOpen(false);
+    }
+
+  return (
+    <div className={styles.formContainer}>
+        <h3>Add Balance</h3>
+        <form onSubmit={handleSubmit}>
+
+            <input 
+                type="number"
+                placeholder="Add Income"
+                value={income} 
+                onChange={(e) => setIncome(e.target.value)}
+                required
+            />
+
+            <Button 
+                type="submit" 
+                style="primary" 
+                shadow
+            >
+                Add Balance
+            </Button>
+
+            <Button
+                style="secondary"
+                shadow
+                handleClick={() => setIsOpen(false)}
+            >
+                Cancel
+            </Button>
+        </form>
+    </div>
+  )
+}
+
+export default AddBalanceForm
