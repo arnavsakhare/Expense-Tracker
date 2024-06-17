ea2edc4d3f97fbcaa8890ffc0e6d0baa4cca780d
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-06-18 02:10:37 +0530

    rendered app into main with SnackBar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SnackbarProvider } from 'notistack'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider
+      maxSnack={1}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "center",
+      }}
+      preventDuplicate
+    >
+      <App />
+    </SnackbarProvider>
+    
   </React.StrictMode>,
 )
